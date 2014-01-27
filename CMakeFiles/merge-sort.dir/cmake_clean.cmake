f16f3185f39e7742ae8bd0972e9ed6e7c9362018
totrit <totrit@gmail.com>
2014-01-27 15:49:56 +0800

    total sorting, not the MERGING-TWO-PRE-SORTED sorting.

---
+++ CMakeFiles/merge-sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/merge-sort.dir/src/merge-sort.cpp.o"
+  "merge-sort.pdb"
+  "merge-sort"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/merge-sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
