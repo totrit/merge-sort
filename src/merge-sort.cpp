6e48d2748b57c7e02f67fa91f44517462b05c025
totrit <totrit@gmail.com>
2014-01-27 15:49:56 +0800

    total sorting, not the MERGING-TWO-PRE-SORTED sorting.

--- src/merge-sort.cpp
+++ src/merge-sort.cpp
@@ -1,63 +1,74 @@
+
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #include <algorithm>
 #include <iostream>
 #include <vector>
 
-int* merge_sort(int array1[], int len1, int array2[], int len2) {
-    int *array_ret = new int[len1 + len2];
-    int cursor1 = 0, cursor2 = 0;
+int* merge_sort(int *array, int len) {
+	if (len <= 1) {
+		return array;
+	}
+	int len1 = len / 2;
+	int len2 = len - len1;
+	int *array1 = array;
+	int *array2 = array + len1;
+	array1 = merge_sort(array1, len1);
+	array2 = merge_sort(array2, len2);
+	int *array_result = new int[len];
+	// Do the merge.
+	int cursor1 = 0, cursor2 = 0;
     while (cursor1 < len1 && cursor2 < len2) {
 		if (array1[cursor1] > array2[cursor2]) {
-			array_ret[cursor1 + cursor2] = array2[cursor2];
+			array_result[cursor1 + cursor2] = array2[cursor2];
 			cursor2 ++;
 		} else {
-			array_ret[cursor1 + cursor2] = array1[cursor1];
+			array_result[cursor1 + cursor2] = array1[cursor1];
 			cursor1 ++;
 		}
 	}
-
+	
 	if (cursor1 < len1) {
 		while (cursor1 < len1) {
-			array_ret[cursor1 + cursor2] = array1[cursor1];
+			array_result[cursor1 + cursor2] = array1[cursor1];
 			cursor1 ++;
 		}
 	}
 
 	if (cursor2 < len2) {
 		while (cursor2 < len2) {
-			array_ret[cursor1 + cursor2] = array2[cursor2];
+			array_result[cursor1 + cursor2] = array2[cursor2];
 			cursor2 ++;
 		}
 	}
 
+    //Copy the sorted elements back into the source array
+    memcpy((void*)array, (void*)array_result, len * sizeof(int));
+    delete []array_result;
+
 	assert(cursor1 == len1 || cursor1 == 0);
 	assert(cursor2 == len2 || cursor2 == 0);
-	return array_ret;
+	return array;
 }
 
 int main() {
 	srand(time(NULL));
-	const int array1_size = 20;
-	const int array2_size = 30;
-	const int num_span = 10;
-	int array1[array1_size];
-	int array2[array2_size];
-	std::vector<int> std_result(50);
-	for (int i = 0; i < array1_size; i ++) {
-		array1[i] = i * num_span + rand() % num_span;
+	int tested_array_len = 10000;
+	int *array = new int[tested_array_len];
+	for (int i = 0; i < tested_array_len; i ++) {
+		array[i] = rand() % 100000;
 	}
-	for (int i = 0; i < array2_size; i ++) {
-		array2[i] = i * num_span + rand() % num_span;
+	std::vector<int> v(array, array + tested_array_len);
+	std::sort(v.begin(), v.end());
+	int *ret = merge_sort(array, tested_array_len);
+	
+	for (int i = 0; i < tested_array_len; i ++) {
+		assert(ret[i] == v[i]);
+		std::cout << ret[i] << ", ";
 	}
-	std::merge(array1, array1 + array1_size, array2, array2 + array2_size, std_result.begin());
-
-	int *ret = merge_sort(array1, array1_size, array2, array2_size);
-
-	for (int i = 0; i < array1_size + array2_size; i ++) {
-		assert(ret[i] == std_result[i]);
-	}
+	std::cout << "OK" << std::endl;
 	return 0;
 }
